c86ed433c768e085bf314671f40eac24a53a41e2
Pratik Kulkarni <pratik.kul5533@gmail.com>
2017-06-28 14:22:07 -0400

    using world-bank

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 var ArgumentParser = require('argparse').ArgumentParser;
 var bluebird = require('bluebird')
 const async = require('async');
+var csvtojson = require('csvtojson')
+var readFile = bluebird.promisify(fs.readFile)
+var readDir = bluebird.promisify(fs.readdirAsync)
+const jsonfile = bluebird.promisify(require('jsonfile').readFile);
 
 var parser = new ArgumentParser({
   version: '0.0.1',
@@ -54,15 +58,57 @@ let mosquito = {}
 
 async.waterfall([
   // get population and fill in population object
+  // (callback) => {
+  //   main.getPopulationByKey(getConfig('population', 'path'))
+  //   .then(content => {
+  //     Object.assign(population, content)
+  //     callback(null)
+  //   })
+  // },
   (callback) => {
-    main.getPopulationByKey(getConfig('population', 'path'))
+    let tempPopulation = {}
+    readFile('./POP.csv', 'utf8')
     .then(content => {
-      Object.assign(population, content)
-      callback(null)
+      csvtojson({noheader:true})
+      .fromString(content)
+      .on('json',(json) => {
+        let country = json.field1.toLowerCase()
+        if (!(country in population)) {
+          let pop = parseInt(json.field5.replace(/,/g, '')) * 1000
+          tempPopulation[country] = [{ sum: pop }]
+        }
+      })
+      .on('done',()=>{
+        Object.assign(population, tempPopulation)
+        callback(null)
+      })
+    })
+  },
+  (callback) => {
+    readDir(getConfig('shape_files'))
+    .then(countries => {
+      bluebird.each(countries, country => {
+        readFile(getConfig('shape_files') + country + '/' + `${country}_adm0.csv`, 'utf8')
+        .then(content => {
+          csvtojson()
+          .fromString(content)
+          .on('json',(json) => {
+            let key = json.ISO.toLowerCase()
+            if (key in population) {
+              let area = parseInt(json.SQKM)
+              population[key][0].sq_km = area
+            }
+          })
+          .on('done',()=>{
+            callback(null)
+          })
+        })
+      })
     })
   },
   // get mosquito prevelence and fill in mosquito object
   (callback) => {
+    console.log(population);
     main.getMosquito(getConfig('aegypti', 'path'))
     .then(content => {
       Object.assign(mosquito, content)
@@ -76,7 +122,7 @@ async.waterfall([
       .then((risk) => {
         // write it in a file at output_path/disease/date.json
         console.log(`writting ${date}.json`);
-        fs.writeFileSync(`${getConfig('output_path')}/${disease}/${date}.json`, JSON.stringify(risk[date]));
+        fs.writeFileSync(`${getConfig('output_path')}/${disease}-worldbank/${date}.json`, JSON.stringify(risk[date]));
       });
     }, {concurency: 1})
   }
